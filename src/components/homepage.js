8eed6ae500c3d42f600869ba00921f0d73994840
dbartleman <drewbartleman@gmail.com>
2020-09-29 00:42:52 -0700

    homepage style and element layout done; no functionality yet

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import '../stylesheets/homepage.css';
 
 export default function Homepage() {
     return(
-        <div />
+        <div className={"homepageContainer"}>
+        <div class={"sidebarLeft"}>
+          <button id={"tab1"}
+            type="button">
+            Notes
+          </button>
+          <button id={"tab2"}
+            type="button">
+            Reminders
+          </button>
+        </div>
+        <div class={"noteArea"}>
+          <input type={"text"} id={"noteTitle"} value={"Title"}/>
+          <input type={"text"} id={"noteText"} value={"Enter note text here..."}/>
+          <div className={"buttons"}>
+            <button id={"postNote"}
+              type="button">
+              Post Note
+            </button>
+            <button id={"deleteNote"}
+              type="button">
+              Delete Note
+            </button>
+          </div>
+        </div>
+      </div>
     );
 }
 
