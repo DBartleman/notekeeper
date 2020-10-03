128c4a8747effedf176d0f7b37de4ac6122b44d5
dbartleman <drewbartleman@gmail.com>
2020-10-02 22:31:24 -0700

    add user login reducer (unfinished)

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -4,4 +4,18 @@ const initialState = {
     user: null,
 }
 
-// build out reducer to put user into state
+// the below is based on the example from official react documentation,
+// https://redux.js.org/tutorials/essentials/part-1-overview-concepts
+function receiveUserReducer(state = initialState, action) {
+    // Check to see if the reducer cares about this action
+    if (action.type === RECEIVE_USER) {
+        // If so, make a copy of `state`
+        return {
+            ...state,
+            // and update the copy with the new value
+            user: "Drew" // placeholder
+        }
+    }
+    // otherwise return the existing state unchanged
+    return state
+}
